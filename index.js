38db0e93479b9d548e8e05cb2e2e8b37ffda48be
Will Toozs <cris.hobson@hotmail.com>
2020-11-12 16:50:09 +0100

    trying different upload setup

--- index.js
+++ index.js
@@ -38,13 +38,31 @@ app.post('/upload', upload, (req, res) => {
       const uploadPromise = new AWS.S3({ apiVersion: '2006-03-01' }).putObject(objectParams).promise();
       uploadPromise.then((data) => {
         console.log("Successfully uploaded data to " + bucketName + "/" + req.file.originalname);
-      }).catch((err) => {
-        console.error(err, err.stack);
+      }).catch((error) => {
+        console.error(error, error.stack);
       });
 
     }
   })
 })
+
+
+// TODO RDS Insert
+// app.post('/users', (req, res) => {
+//   if (req.query.username && req.query.email && req.query.age) {
+//       console.log('Request received');
+//       con.connect(function(err) {
+//           con.query(`INSERT INTO main.users (username, email, age) VALUES ('${req.query.username}', '${req.query.email}', '${req.query.age}')`, function(err, result, fields) {
+//               if (err) res.send(err);
+//               if (result) res.send({username: req.query.username, email: req.query.email, age: req.query.age});
+//               if (fields) console.log(fields);
+//           });
+//       });
+//   } else {
+//       console.log('Missing a parameter');
+//   }
+// });
+
 
 
 // Create unique bucket name
