cfa4de08e24ea7d856d1c7ec26053150b83dc0b7
Dzmitry_Navitski2 <navitskyd@yandex.by>
2021-01-30 00:40:43 +0300

    upload s3 first

--- backend/src/api/services/S3.js
+++ backend/src/api/services/S3.js
@@ -2,18 +2,22 @@ const s3config = require('../../config/S3Config');
 const AWS = require("aws-sdk");
 exports.uploadImageS3 = async (objectParams) => {
 
-  AWS.config.getCredentials(function (err) {
-    if (err) console.log(err.stack);
-    else {
-      console.log("Access key:", AWS.config.credentials.accessKeyId);
+    AWS.config.getCredentials(function (err) {
+        if (err) console.log(err.stack);
+        else {
+            console.log("Access key:", AWS.config.credentials.accessKeyId);
+        }
+    });
+
+    let response = await new AWS.S3().listBuckets().promise();
+    let buckets = response['Buckets'];
+    // console.log(buckets);
+    // console.log(s3config.bucket.config.params['Bucket']);
+
+    if (!buckets.some((bucket) => bucket['Name'] === s3config.bucket.config.params['Bucket'])) {
+        console.log('NO BUCKET WITH NAME ' + s3config.bucket.config.params['Bucket']);
     }
-  });
 
-  let response =  await new AWS.S3().listBuckets().promise();
-  let buckets = response['Buckets'];
-  console.log(buckets);
-  console.log(s3config.bucket);
-
-  return s3config.bucket.putObject(objectParams).promise();
+    return s3config.bucket.putObject(objectParams).promise();
 }
 
