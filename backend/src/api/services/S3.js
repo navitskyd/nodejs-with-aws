8d341a718e9ab66749296522dd12ebcc7a87b5fe
Dzmitry_Navitski2 <navitskyd@yandex.by>
2021-01-30 00:50:53 +0300

    upload s3 first

--- backend/src/api/services/S3.js
+++ backend/src/api/services/S3.js
@@ -2,7 +2,7 @@ const s3config = require('../../config/S3Config');
 const AWS = require("aws-sdk");
 exports.uploadImageS3 = async (objectParams) => {
 
-    AWS.config.getCredentials(function (err) {
+    AWS.config.getCredentials((err) => {
         if (err) console.log(err.stack);
         else {
             console.log("Access key:", AWS.config.credentials.accessKeyId);
@@ -12,16 +12,16 @@ exports.uploadImageS3 = async (objectParams) => {
     let s3 = new AWS.S3();
     let response = await s3.listBuckets().promise();
     let buckets = response['Buckets'];
-    // console.log(buckets);
-    // console.log(s3config.bucket.config.params['Bucket']);
 
     if (!buckets.some((bucket) => bucket['Name'] === s3config.bucket.config.params['Bucket'])) {
         console.log('NO BUCKET WITH NAME ' + s3config.bucket.config.params['Bucket']);
-        await s3.createBucket({Bucket: s3config.bucket.config.params['Bucket']}, function (err, data) {
+        s3.createBucket({Bucket: s3config.bucket.config.params['Bucket']}, (err, data) => {
             if (err) {
                 console.log(err);
             } else {
                 params = {Bucket: s3config.bucket.config.params['Bucket']};
+                console.log(params);
+                console.log("bucket created! ?>>>");
             }
         });
     }
