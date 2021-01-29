4f6914080da5d9d0fee311ae8fd653d81e196ed4
Dzmitry_Navitski2 <navitskyd@yandex.by>
2021-01-30 00:24:04 +0300

    get list of buckets

--- backend/src/api/services/S3.js
+++ backend/src/api/services/S3.js
@@ -1,6 +1,17 @@
 const s3config = require('../../config/S3Config');
-
+const AWS = require("aws-sdk");
 exports.uploadImageS3 = async (objectParams) => {
+
+  AWS.config.getCredentials(function (err) {
+    if (err) console.log(err.stack);
+    else {
+      console.log("Access key:", AWS.config.credentials.accessKeyId);
+    }
+  });
+
+  let buckets = new AWS.S3().list();
+  console.log(buckets);
+
   return s3config.bucket.putObject(objectParams).promise();
 }
 
