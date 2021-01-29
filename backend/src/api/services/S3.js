3800fbf38b5f2f957b8f2b26783b20cec84a19fa
Dzmitry_Navitski2 <navitskyd@yandex.by>
2021-01-30 00:45:50 +0300

    upload s3 first

--- backend/src/api/services/S3.js
+++ backend/src/api/services/S3.js
@@ -9,13 +9,21 @@ exports.uploadImageS3 = async (objectParams) => {
         }
     });
 
-    let response = await new AWS.S3().listBuckets().promise();
+    let s3 = new AWS.S3();
+    let response = await s3.listBuckets().promise();
     let buckets = response['Buckets'];
     // console.log(buckets);
     // console.log(s3config.bucket.config.params['Bucket']);
 
     if (!buckets.some((bucket) => bucket['Name'] === s3config.bucket.config.params['Bucket'])) {
         console.log('NO BUCKET WITH NAME ' + s3config.bucket.config.params['Bucket']);
+        s3.createBucket({Bucket: s3config.bucket.config.params['Bucket']}, function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                params = {Bucket: s3config.bucket.config.params['Bucket']};
+            }
+        });
     }
 
     return s3config.bucket.putObject(objectParams).promise();
